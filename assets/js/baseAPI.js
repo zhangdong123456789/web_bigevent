661fd6742d2fd6f2205788a204b68df9081e4a0f
zhang <921511675@qq.com>
2022-02-22 14:35:13 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -17,7 +17,7 @@ $.ajaxPrefilter(function(options) {
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             //1.强制清空token
             localStorage.removeItem('token');
-            // 2.强制跳转页面
+            // 2.强制跳转页面 
             location.href = '/login.html';
         }
     }
